752fd35ed2b042aa7feef3c1e1e1aa149680deda
luz Villar <luzvf18@gmail.com>
2021-05-29 20:22:15 -0500

    ejercicio raindrops

---
+++ raindrops/raindrops.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Raindrops' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const convert = (number) => {
+  let raindrops = '';
+  if (number % 3 == 0) {
+    raindrops += 'Pling'
+  }
+  if (number % 5 == 0) {
+    raindrops += 'Plang'
+  }
+  if (number % 7 == 0) {
+    raindrops += 'Plong'
+  }
+  if (raindrops.length == 0) {
+    raindrops += number;
+  }
+  return raindrops;
+};
