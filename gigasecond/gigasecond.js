81a47e12e2dd7192a02f26da78399ea2f1814f19
luz Villar <luzvf18@gmail.com>
2021-05-29 20:20:08 -0500

    ejercicio gigasecond

---
+++ gigasecond/gigasecond.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (date) => {
+  let newDate = new Date(date);
+  newDate.setSeconds(date.getSeconds() + 1000000000);
+  return newDate;
+};
